d8b450fb3a72d577fc3124902f3aaf7949ff58ef
Larissa Brito <larissa0britorlgmail.com>
2022-11-30 19:11:55 -0300

    Criação das rotas de alteração e deleção da API do mongodb

---
+++ MongoDB/api.py
@@ -0,0 +1,84 @@
+from flask import Flask, request
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+conn = MongoClient(
+    'mongodb+srv://gamamagalu.jl8mrli.mongodb.net/projeto_3',
+
+    username='gabisilva',
+    password='100113'
+)
+db = conn['projeto_3']
+
+
+@app.route('/')
+def home():
+    return "Olá, to funfando"
+
+
+# Rota de cadastro
+'''{"id":1,
+"nome": "Tomate",
+"preco":5.00,
+"descricao":"Tomate eh fruta"}'''
+
+
+@app.route('/cadastro', methods=['POST'])
+def cadastrar():
+    if request.get_json(silent=True):
+        produto = request.json
+        db.produtos.insert_one(produto)
+        return produto, 201
+    else:
+        return {'erro': 'Esperava receber um json no corpo da requisição'}, 400
+
+
+# Rota de alteração
+
+@app.route('/atualizacao/<nome>', methods=['PUT'])
+def atualizar(nome):
+    produto, status = consultar(nome)
+    if status == 200:
+        request.get_json(silent=True)
+        produto = request.json
+        db.produtos.update_one(
+            {'nome': produto['nome']},
+            {'$set':
+                {
+                'nome': produto['nome'],
+                'preco': produto['preco'],
+                'descricao': produto['descricao']
+                }
+            }
+        )
+        return produto, 200
+    else:
+        return {'erro': "Produto não encontrado!"}, 404
+
+
+# Rota de consulta)
+@app.route('/consulta/<nome>', methods=['GET'])
+def consultar(nome):
+    produto = db.produtos.find_one({'nome': nome})
+    if produto:
+        return produto, 200
+    else:
+        return {'error': 'Produto não encontrado!'}, 404
+
+
+# Rota de deleção
+
+@app.route('/delecao/<nome>', methods=['DELETE'])
+def deletar(nome):
+    produto = db.produtos.find_one({'nome': nome})
+    if produto:
+        db.produtos.delete_one({'nome': nome})
+        return {'message': 'Produto deletado com sucesso!'}, 200
+    else:
+        return {'error': 'Produto não encontrado!'}, 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
